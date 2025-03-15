ecac28c23431949b1c14b0c235602bd42003f001
mallikanicole <malmichael23@gmail.com>
2025-03-15 11:11:59 -0400

    test2

--- cc_15.js
+++ cc_15.js
@@ -2,6 +2,8 @@
 function addRiskItem(riskName, riskLevel, department) {
     const card = document.createElement("div");
     ticket.classList.add("risk-card");
+    const riskitem = document.createElement('div');
+    ticketCard.setAttribute('class','risk-card');
 
     const nameHeading = document.createElement("h3");
     nameHeading.textContent = riskName;
